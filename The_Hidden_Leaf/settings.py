d40154d3c15965686a609b1cfccb6c55aec64401
Anuj Pandey <anujpandey785@gmail.com>
2020-08-23 23:21:29 +0530

    django heroku with psycopg2

--- The_Hidden_Leaf/settings.py
+++ The_Hidden_Leaf/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 from decouple import config
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -142,3 +143,5 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 MEDIA_URL = '/media/'
+
+django_heroku.settings(locals())
