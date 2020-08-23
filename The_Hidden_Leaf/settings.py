17abcc61c1db6d74366e694d6161ea0aac49b697
Anuj Pandey <anujpandey785@gmail.com>
2020-08-23 22:27:51 +0530

    using environment variables

--- The_Hidden_Leaf/settings.py
+++ The_Hidden_Leaf/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +21,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cq)(5uziz823rdbe%^bg9khf)fa%q10n9l088rjv2cn^&^9p9a'
+SECRET_KEY = config('SECRET_KEY',default='')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG',cast=bool,default =True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    host.strip()
+    for host in config('ALLOWED_HOSTS' , default='').split(',')
+]
 
 
 # Application definition
